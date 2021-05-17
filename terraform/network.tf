4007a9cde7e0374cb1bd9fbd3efcefb1091c8f3e
Matt Merrill <392778+theothermattm@users.noreply.github.com>
2021-05-17 14:38:59 -0400

    Adding in security group

--- terraform/network.tf
+++ terraform/network.tf
@@ -106,11 +106,10 @@ resource "aws_security_group" "ecs_tasks" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    protocol         = "tcp"
-    from_port        = var.container_port
-    to_port          = var.container_port
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    protocol        = "tcp"
+    from_port       = var.container_port
+    to_port         = var.container_port
+    security_groups = [aws_security_group.alb.id]
   }
 
   egress {
