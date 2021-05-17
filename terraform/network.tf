75740f98af8c2a0dcdaeb9c134ae17b1a5118050
Matt Merrill <392778+theothermattm@users.noreply.github.com>
2021-05-17 14:27:09 -0400

    Adding fix for egress, didn't work.

--- terraform/network.tf
+++ terraform/network.tf
@@ -92,7 +92,13 @@ resource "aws_security_group" "alb" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
+  egress {
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 resource "aws_security_group" "ecs_tasks" {
