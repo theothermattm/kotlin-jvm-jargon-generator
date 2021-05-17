ce8e4ae92ff86a7b34f6d650360ffaddb24eeae5
Matt Merrill <392778+theothermattm@users.noreply.github.com>
2021-05-17 11:11:28 -0400

    Adding terraform templates for ECS

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,26 +1,182 @@
-resource "aws_ecr_repository" "worker" {
-  name = "worker"
+resource "aws_ecr_repository" "main" {
+  name                 = "${var.name}-${var.environment}"
+  image_tag_mutability = "MUTABLE"
 }
 
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "terraform-learning-cluster"
+resource "aws_ecr_lifecycle_policy" "main" {
+  repository = aws_ecr_repository.main.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "keep last 10 images"
+      action = {
+        type = "expire"
+      }
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 10
+      }
+    }]
+  })
 }
 
-data "template_file" "task_definition_template" {
-  template = file("${path.module}/task_definition.json.tpl")
-  vars = {
-    REPOSITORY_URL = replace(aws_ecr_repository.worker.repository_url, "https://", "")
+resource "aws_ecs_cluster" "main" {
+  name = "${var.name}-cluster-${var.environment}"
+}
+
+resource "aws_cloudwatch_log_group" "ecs_log_group" {
+  name = "${var.name}-ecs-logs-${var.environment}"
+  tags = {
+    Name = "${var.name}-ecs-logs-${var.environment}"
+  }
+}
+resource "aws_ecs_task_definition" "main" {
+  network_mode             = "awsvpc"
+  family                   = "terraformlearning"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  container_definitions = jsonencode([{
+    name      = "${var.name}-container-${var.environment}"
+    image     = "${var.container_image}:latest"
+    essential = true
+    //environment = var.environment
+    portMappings = [{
+      protocol      = "tcp"
+      containerPort = var.container_port
+      hostPort      = var.container_port
+    }]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "${var.name}-ecs-logs-${var.environment}"
+        awslogs-region        = "us-east-2",
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.name}-ecsTaskRole"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "dynamodb" {
+  name        = "${var.name}-task-policy-dynamodb"
+  description = "Policy that allows access to DynamoDB"
+
+  policy = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [
+       {
+           "Effect": "Allow",
+           "Action": [
+               "dynamodb:CreateTable",
+               "dynamodb:UpdateTimeToLive",
+               "dynamodb:PutItem",
+               "dynamodb:DescribeTable",
+               "dynamodb:ListTables",
+               "dynamodb:DeleteItem",
+               "dynamodb:GetItem",
+               "dynamodb:Scan",
+               "dynamodb:Query",
+               "dynamodb:UpdateItem",
+               "dynamodb:UpdateTable"
+           ],
+           "Resource": "*"
+       }
+   ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.dynamodb.arn
+}
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.name}-ecsTaskExecutionRole"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_subnet_ids" "public_subnet_ids" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Tier = "public"
   }
 }
 
-resource "aws_ecs_task_definition" "task_definition" {
-  family                = "worker"
-  container_definitions = data.template_file.task_definition_template.rendered
+data "aws_security_groups" "service_sg" {
+  filter {
+    name   = "group-name"
+    values = ["${var.name}-sg-task-${var.environment}"]
+  }
 }
 
-resource "aws_ecs_service" "worker" {
-  name            = "worker"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.task_definition.arn
-  desired_count   = 2
+resource "aws_ecs_service" "main" {
+  name                               = "${var.name}-service-${var.environment}"
+  cluster                            = aws_ecs_cluster.main.id
+  task_definition                    = aws_ecs_task_definition.main.arn
+  desired_count                      = 2
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+  launch_type                        = "FARGATE"
+  scheduling_strategy                = "REPLICA"
+
+  network_configuration {
+    security_groups  = data.aws_security_groups.service_sg.ids
+    subnets          = data.aws_subnet_ids.public_subnet_ids.ids
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.main.arn
+    container_name   = "${var.name}-container-${var.environment}"
+    container_port   = var.container_port
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition, desired_count]
+  }
 }
--- terraform/network.tf
+++ terraform/network.tf
@@ -1,30 +1,52 @@
-
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.terraform_learning_vpc.id
+resource "aws_lb" "main" {
+  name                       = "${var.name}-alb-${var.environment}"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.alb.id]
+  subnets                    = aws_subnet.public.*.id
+  enable_deletion_protection = false
 }
 
-resource "aws_subnet" "pub_subnet" {
-  vpc_id            = aws_vpc.terraform_learning_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-2b"
-}
+resource "aws_alb_target_group" "main" {
+  name        = "${var.name}-tg-${var.environment}"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
 
-resource "aws_subnet" "pub_subnet2" {
-  vpc_id            = aws_vpc.terraform_learning_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-2a"
-}
-
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.terraform_learning_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway.id
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = "/"
+    unhealthy_threshold = "2"
   }
 }
 
-resource "aws_route_table_association" "route_table_association" {
-  subnet_id      = aws_subnet.pub_subnet.id
-  route_table_id = aws_route_table.public.id
+resource "aws_alb_listener" "http" {
+  load_balancer_arn = aws_lb.main.id
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    target_group_arn = aws_alb_target_group.main.id
+    type             = "forward"
+  }
 }
+
+/*
+resource "aws_alb_listener" "https" {
+  load_balancer_arn = aws_lb.main.id
+  port              = 443
+  protocol          = "HTTPS"
+ 
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.alb_tls_cert_arn
+ 
+  default_action {
+    target_group_arn = aws_alb_target_group.main.id
+    type             = "forward"
+  }
+}
+*/
